e4c7876a860442d2dccd109a27041b4a379bfc1f
rusli abdul gani <rusli.gani88@gmail.com>
2017-07-13 21:26:57 +0700

    baru bisa validasi format email T-T

--- models/student.js
+++ models/student.js
@@ -3,7 +3,15 @@ module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Format email salah'
+        }
+      }
+    },
     jurusan: DataTypes.STRING
   }, {
     classMethods: {
