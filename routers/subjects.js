b29ab71a3e3b4e62a8cad73074d7fd30b8cfdd31
rusli abdul gani <rusli.gani88@gmail.com>
2017-07-14 22:03:21 +0700

    belum edit subject

--- routers/subjects.js
+++ routers/subjects.js
@@ -8,11 +8,40 @@ router.get('/', (req, res) => {
     include: [db.Teacher]
   })
   .then( data => {
-      res.render('subjects', {header: 'Subject Page', data_subject: data})
+      res.render('subjects', {header: 'Subject Page', data_subject: data, data_kosong: 'Belum ada guru'})
       console.log(`--------------------- ${data[0].Teachers[0].first_name}`);
     })
 })
 
+router.post('/', (req, res) => {
+  db.Subject.create(req.body)
+  .then( data => {
+    res.redirect('/subjects')
+  })
+})
+
+router.get('/edit/:id', (req, res) => {
+  let id = req.params.id
+  db.Subject.findById(id)
+  .then( data => {
+    res.render('edit_subject', {header: 'Edit Subject Page', data_subject : data})
+    console.log(`-------------------------- ${data.id}`);
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  let id =  req.params.id
+  let data = req.body
+  db.Subject.update({data}, {
+    where: {id:id}
+  })
+  .then( ()=>{
+    res.redirect('/subjects')
+  })
+  .catch( err => {
+    console.log(err);
+  })
+})
 
 
 
