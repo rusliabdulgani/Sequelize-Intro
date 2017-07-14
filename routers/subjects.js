3bc88070954454fced6c8655f310d217e23c7ecd
rusli abdul gani <rusli.gani88@gmail.com>
2017-07-14 21:28:54 +0700

    show tabel subject dengan tiap-tiap teacher yg terkait

--- routers/subjects.js
+++ routers/subjects.js
@@ -4,11 +4,17 @@ const db = require('../models');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  db.Subject.findAll()
-  .then( data => {
-    res.render('subjects', {header: 'Subject Page', data_subjects: data})
+  db.Subject.findAll({
+    include: [db.Teacher]
   })
+  .then( data => {
+      res.render('subjects', {header: 'Subject Page', data_subject: data})
+      console.log(`--------------------- ${data[0].Teachers[0].first_name}`);
+    })
 })
+
+
+
 
 
 
